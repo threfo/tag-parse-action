3f47db90df8cb79921ff001629cc397b70c866a8
Thomas <66234756+thomas-bello@users.noreply.github.com>
2023-05-24 14:53:01 +0800

    fix: 添加runCommand

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,52 +2,52 @@
 /* eslint-disable github/array-foreach */
 
 export const getBranchByHead = (ref: string): string => {
-  if (ref.includes('refs/heads/')) {
-    return ref.replace('refs/heads/', '')
+  if (ref.includes("refs/heads/")) {
+    return ref.replace("refs/heads/", "");
   }
-  return ''
-}
+  return "";
+};
 
 export const getBranchByTag = (ref: string): string => {
-  if (ref.includes('refs/tags/release/')) {
-    const commitMsg = ref.replace('refs/tags/', '')
-    const index = commitMsg.lastIndexOf('-v')
-    return commitMsg.slice(0, index)
+  if (ref.includes("refs/tags/release/")) {
+    const commitMsg = ref.replace("refs/tags/", "");
+    const index = commitMsg.lastIndexOf("-v");
+    return commitMsg.slice(0, index);
   }
-  return ''
-}
+  return "";
+};
 
 export const getPraseByTag = (ref: string): {} => {
-  if (ref.includes('refs/tags/release/')) {
-    const willString = ref.replace('refs/tags/release/', '')
-    const arr = (willString || '').split('&')
-    const obj: any = {}
-    arr.forEach(item => {
-      const [key, value] = (item || '').split('=')
+  if (ref.includes("refs/tags/release/")) {
+    const willString = ref.replace("refs/tags/release/", "");
+    const arr = (willString || "").split("&");
+    const obj: any = {};
+    arr.forEach((item) => {
+      const [key, value] = (item || "").split("=");
       if (value) {
-        obj[key] = value
+        obj[key] = value;
       }
-    })
-    return obj
+    });
+    return obj;
   }
-  return {}
-}
+  return {};
+};
 export const getTagUrl = (repository: string): string => {
-  return `https://api.github.com/repos/${repository}/releases`
-}
+  return `https://api.github.com/repos/${repository}/releases`;
+};
 
 // release/dingding-dev-v0.1.3-2021-12-06
 export const getSyncBranch = (ref: string): string => {
-  if (ref.includes('refs/heads/')) {
-    return ref.replace('refs/heads/', '')
+  if (ref.includes("refs/heads/")) {
+    return ref.replace("refs/heads/", "");
   }
-  if (ref.includes('refs/tags/release/')) {
-    const commitMsg = ref.replace('refs/tags/', '')
-    const index = commitMsg.lastIndexOf('-dev-v')
-    return commitMsg.slice(0, index)
+  if (ref.includes("refs/tags/release/")) {
+    const commitMsg = ref.replace("refs/tags/", "");
+    const index = commitMsg.lastIndexOf("-dev-v");
+    return commitMsg.slice(0, index);
   }
-  return ''
-}
+  return "";
+};
 /**
  * 格式化时间
  *
@@ -58,17 +58,17 @@ export const getSyncBranch = (ref: string): string => {
  * @example formatTime('2018-1-29', '{y}/{m}/{d} {h}:{i}:{s}') // -> 2018/01/29 00:00:00
  */
 export const formatTime = (dateTime: any, cFormat: string): string => {
-  let time = dateTime
+  let time = dateTime;
   if (`${time}`.length === 10) {
-    time = +time * 1000
+    time = +time * 1000;
   }
 
-  const format = cFormat || '{y}-{m}-{d} {h}:{i}:{s}'
-  let date
-  if (typeof time === 'object') {
-    date = time
+  const format = cFormat || "{y}-{m}-{d} {h}:{i}:{s}";
+  let date;
+  if (typeof time === "object") {
+    date = time;
   } else {
-    date = new Date(time)
+    date = new Date(time);
   }
 
   const formatObj: any = {
@@ -79,16 +79,16 @@ export const formatTime = (dateTime: any, cFormat: string): string => {
     h: date.getHours(),
     i: date.getMinutes(),
     s: date.getSeconds(),
-    a: date.getDay()
-  }
+    a: date.getDay(),
+  };
   const time_str = format.replace(/{(y|m|d|h|i|s|a)+}/g, (result, key) => {
-    let value = formatObj[key]
-    if (key === 'a')
-      return ['一', '二', '三', '四', '五', '六', '日'][value - 1]
+    let value = formatObj[key];
+    if (key === "a")
+      return ["一", "二", "三", "四", "五", "六", "日"][value - 1];
     if (result.length > 0 && value < 10) {
-      value = `0${value}`
+      value = `0${value}`;
     }
-    return value || 0
-  })
-  return time_str
-}
+    return value || 0;
+  });
+  return time_str;
+};
